ec4123b21b727d80457b6d3b09e8c5e5c11b0b80
Kornelius <magnus.korneliusson@gmail.com>
2023-11-30 23:11:55 +0100

    Initial commit.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using AoCHelper;
+
+namespace AOC_2023;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Solver.SolveLast();
+    }
+}
