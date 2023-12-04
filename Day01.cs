d544c5d58923fa05ef208efb2d3429935cb2785b
Kornelius <magnus.korneliusson@gmail.com>
2023-12-04 13:34:39 +0100

    Simplified day 1.

--- Day01.cs
+++ Day01.cs
@@ -16,43 +16,42 @@ public class Day01 : BaseDay
         int sum = 0;
         foreach (string line in _input.Split('\n'))
         {
-            var nums = line.Where(char.IsDigit).Select(c => c - '0').ToArray();
-            // Console.WriteLine(s + " " + string.Join(", ", nums) + $" -> {nums[0] * 10 + nums[^1]}");
-            sum += 10 * nums[0] + nums[^1]; // add upp first and last element of nums.
+            var (fst, lst) = FindFirstAndLastDigit(line);
+            sum += 10 * fst + lst;
         }
         return new(sum.ToString());
     }
+
+    private static (int, int) FindFirstAndLastDigit(string line)
+    {
+        int fst = line.First(char.IsDigit) - '0';
+        int lst = line.Last(char.IsDigit) - '0';
+        return (fst, lst);
+    }
+
     public override ValueTask<string> Solve_2()
     {
         int sum = 0;
-        var digits = new List<string>(new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" });
-
-        foreach (string _line in _input.Split("\n"))
+        var digits = new Dictionary<string, string>() {{"one","o1e"},
+                                                       {"two", "t2o"},
+                                                       {"three","thr3e"},
+                                                       {"four","fo4r"},
+                                                       {"five","fi5e"},
+                                                       {"six","s6x"},
+                                                       {"seven","sev7n"},
+                                                       {"eight","e8ght"},
+                                                       {"nine","ni9e"},
+                                                      };
+        List<string> lines = new(_input.Split('\n'));
+        for (int i = 0; i < lines.Count; ++i)
         {
-            string line = _line; // not allowed to modify the foreach-variable...
-            int i = 0;
-            while (i < line.Length)
+            foreach (string digit in digits.Keys)
             {
-                foreach (string digit in digits)
-                {
-                    if (line[i..].StartsWith(digit))
-                    {
-                        // Apparently we don't consume the characters of the spelled out digits.
-                        // This means that "eightwone" -> "8igh2w1ne" and not "8w1" which would
-                        // happen if "eight" was consumed into "8" and "one" into "1".
-                        // Instead we just change the first character of the spelled out digit
-                        // to the digits number.
-                        line = line[..i] + (digits.IndexOf(digit) + 1) + line[(i + 1)..];
-                        // f = f.Insert(i, (digits.IndexOf(digit) + 1).ToString()).Remove(i + 1, 1);
-                        break;
-                    }
-                }
-                i++;
+                lines[i] = lines[i].Replace(digit, digits[digit]);
             }
+            var (fst, lst) = FindFirstAndLastDigit(lines[i]);
+            sum += 10 * fst + lst;
 
-            var nums = line.Where(char.IsDigit).Select(c => c - '0').ToArray();
-            // Console.WriteLine(s + " " + f + " " + string.Join(", ", nums) + $" -> {nums[0] * 10 + nums[^1]}");
-            sum += 10 * nums[0] + nums[^1];
         }
         return new(sum.ToString());
     }
